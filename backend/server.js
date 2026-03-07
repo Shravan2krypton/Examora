b6b5811523ece2fb389c7fd43202ab670f411faa
Shravan2krypton <shraton1102@gmail.com>
2026-03-07 21:31:30 +0530

    Convert socket.js to ES modules and update server import

--- backend/server.js
+++ backend/server.js
@@ -64,24 +64,8 @@ app.use((err, req, res, next) => {
   }
 });
 
-// Initialize Socket.io
-import { Server } from 'socket.io';
-const io = new Server(httpServer, {
-  cors: {
-    origin: true,
-    credentials: true
-  }
-});
-
-io.on('connection', (socket) => {
-  socket.on('join-exam', (examId) => {
-    socket.join(`exam-${examId}`);
-  });
-
-  socket.on('leave-exam', (examId) => {
-    socket.leave(`exam-${examId}`);
-  });
-});
+import { initSocket } from './socket.js';
+initSocket(httpServer);
 
 const PORT = process.env.PORT || 5000;
 httpServer.listen(PORT, () => {
--- backend/socket.js
+++ backend/socket.js
@@ -1,29 +1,28 @@
-const { Server } = require('socket.io');
+import { Server } from 'socket.io';
 
 let io = null;
 
-module.exports = {
-  initSocket: (httpServer) => {
-    io = new Server(httpServer, {
-      cors: {
-        origin: true,
-        credentials: true
-      }
+export const initSocket = (httpServer) => {
+  io = new Server(httpServer, {
+    cors: {
+      origin: true,
+      credentials: true
+    }
+  });
+
+  io.on('connection', (socket) => {
+    socket.on('join-exam', (examId) => {
+      socket.join(`exam-${examId}`);
     });
 
-    io.on('connection', (socket) => {
-      socket.on('join-exam', (examId) => {
-        socket.join(`exam-${examId}`);
-      });
-
-      socket.on('leave-exam', (examId) => {
-        socket.leave(`exam-${examId}`);
-      });
-
-      socket.on('disconnect', () => {});
+    socket.on('leave-exam', (examId) => {
+      socket.leave(`exam-${examId}`);
     });
 
-    return io;
-  },
-  getIO: () => io
+    socket.on('disconnect', () => {});
+  });
+
+  return io;
 };
+
+export const getIO = () => io;
